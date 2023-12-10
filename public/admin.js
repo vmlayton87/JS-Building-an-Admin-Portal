02cf565affcb70a44e05092452be46060248c981
vmlayton87 <vmlayton87@gmail.com>
2023-12-09 22:59:35 -0500

    solution no bonus

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,42 @@
 
 // Your Code Here
+
+
+ // retrieve a list of books
+
+ async function main(){
+    let getBookList = await fetch(`http://localhost:3001/listBooks`)
+    let dataList = await getBookList.json()
+    dataList.forEach(displayBook)
+    console.log(dataList)
+ }
+
+ function displayBook(book) {
+    const root = document.getElementById("root")
+    const li = document.createElement("li")
+    li.textContent = book.title
+    root.append(li)
+
+    const input = document.createElement("input")
+    input.value = book.quantity
+    
+
+    const button = document.createElement("button")
+    button.textContent = "Save"
+    li.append(input, button)
+    
+    button.addEventListener("click", async ()=>{
+        await fetch(`http://localhost:3001/updateBook`, {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json"
+            }, 
+            body: JSON.stringify({
+                id: book.id,
+                quantity: input.value
+            })
+        })
+    })
+ }
+
+ main()
